2d6ea510b44224e460444f9f4d376ca3d47ec11c
sonyasheynav <168831038+sonyasheynav@users.noreply.github.com>
2024-05-06 08:39:34 -0400

    Day 1 Commit

---
+++ bird.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Ninja:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        #add jump sprite too
+        self.image_list = ["images/Run_000.png", "images/Run_004.png", ]
+        self.image = pygame.image.load(self.image_list[0])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 2
+        self.up = True
+
+    def rescale_image(self, image):
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .7, self.image_size[1] * .7)
+        self.image = pygame.transform.scale(self.image, scale_size)
+
+    def move_bird(self):
+        self.x = self.x - self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def switch_image(self):
+        image_number = 0
+        if not self.up:
+            image_number = 1
+        self.image = pygame.image.load(self.image_list[image_number])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.up = not self.up
+
+
--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# set up pygame modules
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 20)
+pygame.display.set_caption("Desert Ninja")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# set up variables for the display
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+BIRD_START_X = 500
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bg = pygame.image.load("desert-background.png")
+house = pygame.image.load("house.png")
+tree = pygame.image.load("tree.png")
+
+
+b = Bird(BIRD_START_X, 250)
+b = Balloon(300, 200)
+
+
+INITIAL_HOUSE_X = random.randint(0,600)
+INITIAL_TREE_X = random.randint(0,600)
+house_x = INITIAL_HOUSE_X
+tree_x = INITIAL_TREE_X
+
+
+# render the text for later
+
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
+run = True
+# -------- Main Program Loop -----------
+clock = pygame.time.Clock()
+frame = 0
+while run:
+    # --- Main event loop
+    clock.tick(60)
+    if frame % 30 == 0:
+        bird.switch_image()
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    screen.blit(bg, (0, 0))
+    screen.blit(house, (house_x, 360))
+    screen.blit(tree, (tree_x, 360))
+    screen.blit(bird.image, bird.rect)
+    screen.blit(b.image, b.rect)
+    pygame.display.update()
+
+    frame += 1
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+

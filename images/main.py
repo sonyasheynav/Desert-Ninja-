d6e95119d537aea1882fd19db02017f0ece1b1ba
sonyasheynav <168831038+sonyasheynav@users.noreply.github.com>
2024-05-08 08:42:52 -0400

    commit day 3
    I resized all my  images and continued to work on the running look of the ninja and his ability to move across the screen in the ninja class.

---
+++ images/main.py
@@ -0,0 +1,63 @@
+import pygame
+import random
+from ninja import Ninja
+
+
+# set up pygame modules
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 20)
+pygame.display.set_caption("Desert Ninja")
+
+
+
+
+# set up variables for the display
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+
+bg_size= (800,600)
+bg = pygame.image.load("images/desert-background.png")
+bg= pygame.transform.scale(bg, bg_size)
+bg_position = (0,0)
+cactus = pygame.image.load("images/cactus.png")
+cactus_pair = pygame.image.load("images/cactus_pair.png")
+
+
+ninja =  Ninja(100, 350)
+
+
+
+INITIAL_CACTUS_X = random.randint(0,600)
+INITIAL_CACTUS_PAIR_X = random.randint(0,600)
+cactus_x = INITIAL_CACTUS_X
+cactus_pair_x = INITIAL_CACTUS_PAIR_X
+
+
+# render the text for later
+
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
+run = True
+# -------- Main Program Loop -----------
+clock = pygame.time.Clock()
+frame = 0
+while run:
+    # --- Main event loop
+    clock.tick(60)
+    if frame % 30 == 0:
+        ninja.switch_image()
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    screen.blit(bg, bg_position)
+    screen.blit(cactus, (cactus_x, 360))
+    screen.blit(ninja.image, ninja.rect)
+    pygame.display.update()
+
+    frame += 1
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+
---
+++ images/ninja.py
@@ -0,0 +1,36 @@
+import pygame
+class Ninja:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        # add jump sprite too
+        self.image_list = ["images/Run__000.png", "images/Run__004.png", "images/Jump__002.png", "images/Jump__008.png"]
+        self.image = pygame.image.load(self.image_list[0])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 2
+        self.up = True
+
+    def rescale_image(self, image):
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .34, self.image_size[1] * .34)
+        self.image = pygame.transform.scale(self.image, scale_size)
+
+    def move_ninja(self):
+        self.x = self.x - self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def switch_image(self):
+        image_number = 0
+        if not self.up:
+            image_number = 1
+        if self.up:
+            image_number = 2
+        self.image = pygame.image.load(self.image_list[image_number])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.up = not self.up
+
+

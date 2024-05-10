15b5a530efc85ca09fa75662bfc713d6be3b913b
sonyasheynav <168831038+sonyasheynav@users.noreply.github.com>
2024-05-10 08:41:05 -0400

    Commit Day 4
    I kept working on the move_ninja function so that he can move around as well as jump over the cacti and when he jumps he doesnt just stay up in space but he falls down, and even possibly work on double jumps?

--- main.py
+++ main.py
@@ -10,21 +10,29 @@ my_font = pygame.font.SysFont('Arial', 20)
 pygame.display.set_caption("Desert Ninja")
 
 
+
+
 # set up variables for the display
 size = (800, 600)
 screen = pygame.display.set_mode(size)
-BIRD_START_X = 500
 
-bg = pygame.image.load("desert-background.png")
-cactus = pygame.image.load("cactus.png")
-cactus_pair = pygame.image.load("cactus_pair.png")
+bg_size= (800,600)
+bg = pygame.image.load("images/desert-background.png")
+bg= pygame.transform.scale(bg, bg_size)
+bg_position = (0,0)
+cactus_size= (100,350)
+cactus = pygame.image.load("images/cactus.png")
+cactus= pygame.transform.scale(cactus, cactus_size)
+cactus_pair = pygame.image.load("images/cactus_pair.png")
 
 
-ninja= Ninja(BIRD_START_X, 250)
+ninja =  Ninja(100, 350)
+
 
 
 
 INITIAL_CACTUS_X = random.randint(0,600)
+INITIAL_CACTUS_Y = random.randint(500,600)
 INITIAL_CACTUS_PAIR_X = random.randint(0,600)
 cactus_x = INITIAL_CACTUS_X
 cactus_pair_x = INITIAL_CACTUS_PAIR_X
@@ -42,16 +50,20 @@ while run:
     clock.tick(60)
     if frame % 30 == 0:
         ninja.switch_image()
-
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        ninja.move_ninja("right")
+    if keys[pygame.K_w]:
+        ninja.move_ninja("up")
+    else:
+        ninja.move_ninja("down")
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
-    screen.blit(bg, (0, 0))
-    screen.blit(cactus, (house_x, 360))
-    screen.blit(tree, (tree_x, 360))
-    screen.blit(bird.image, bird.rect)
-    screen.blit(b.image, b.rect)
+    screen.blit(bg, bg_position)
+    screen.blit(cactus, (cactus_x, 200))
+    screen.blit(ninja.image, ninja.rect)
     pygame.display.update()
 
     frame += 1
--- ninja.py
+++ ninja.py
@@ -1,13 +1,11 @@
 import pygame
-
-
 class Ninja:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        #add jump sprite too
-        self.image_list = ["images/Run_000.png", "images/Run_004.png", "images/Jump_002", "images/Jump_008"]
+        # add jump sprite too
+        self.image_list = ["images/Run__000.png", "images/Run__004.png", "images/Jump__002.png", "images/Jump__008.png"]
         self.image = pygame.image.load(self.image_list[0])
         self.rescale_image(self.image)
         self.image_size = self.image.get_size()
@@ -17,43 +15,28 @@ class Ninja:
 
     def rescale_image(self, image):
         self.image_size = self.image.get_size()
-        scale_size = (self.image_size[0] * .7, self.image_size[1] * .7)
+        scale_size = (self.image_size[0] * .34, self.image_size[1] * .34)
         self.image = pygame.transform.scale(self.image, scale_size)
 
-    import pygame
+    def move_ninja(self,direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        if direction == "up":
+            self.y = self.y - 2 *  self.delta
+            self.x = self.x + self.delta
+        if direction == "down":
+            self.x = self.x + self.delta +1
 
-    class Ninja:
-
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-            # add jump sprite too
-            self.image_list = ["images/Run_000.png", "images/Run_004.png", ]
-            self.image = pygame.image.load(self.image_list[0])
-            self.rescale_image(self.image)
-            self.image_size = self.image.get_size()
-            self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-            self.delta = 2
-            self.up = True
-
-        def rescale_image(self, image):
-            self.image_size = self.image.get_size()
-            scale_size = (self.image_size[0] * .7, self.image_size[1] * .7)
-            self.image = pygame.transform.scale(self.image, scale_size)
-
-        def move_bird(self):
-            self.x = self.x - self.delta
-            self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-        def switch_image(self):
-            image_number = 0
-            if not self.up:
-                image_number = 1
-            if self.up:
-                image_number = 2
-            self.image = pygame.image.load(self.image_list[image_number])
-            self.rescale_image(self.image)
-            self.image_size = self.image.get_size()
-            self.up = not self.up
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    def switch_image(self):
+        image_number = 0
+        if not self.up:
+            image_number = 1
+        if self.up:
+            image_number = 2
+        self.image = pygame.image.load(self.image_list[image_number])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.up = not self.up
 
 
